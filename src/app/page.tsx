5cb8f42868667a4bd30913c9293f16b2e873a42f
fomativeh <ufomaitive@gmail.com>
2024-06-20 17:47:58 +0100

    aha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-start">
-      <p className="max-w-[80vw] text-white">{p}</p>
+      <p className="max-w-[80vw] text-white">{JSON.stringify(p)}</p>
       {/* <p className="text-white">{JSON.stringify(viewport)}</p>
       <p className="text-white">Is expanded = {isExpanded}</p> */}
       {/* {currentPage == "Home" && (
